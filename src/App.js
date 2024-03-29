5d18a5017d12d5dbec90d1615b22b1f0ad78e889
EsmirKafedzic <kafedzic0@gmail.com>
2024-03-29 23:54:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cards from "./components/Cards";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <button className="btn">Reset</button> */}
+     <h1>Memory game - React</h1>
+     <Cards/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import Card from './Card';
+
+function Cards() {
+
+    const [items, setItems] = useState([
+        { id: 1, img: '/img/html.png', state: "" },
+        { id: 1, img: '/img/html.png', state: "" },
+        { id: 2, img: '/img/css.png', state: "" },
+        { id: 2, img: '/img/css.png', state: "" },
+        { id: 3, img: '/img/js.png', state: "" },
+        { id: 3, img: '/img/js.png', state: "" },
+        { id: 4, img: '/img/scss.png', state: "" },
+        { id: 4, img: '/img/scss.png', state: "" },
+        { id: 5, img: '/img/react.png', state: "" },
+        { id: 5, img: '/img/react.png', state: "" },
+        { id: 6, img: '/img/vue.png', state: "" },
+        { id: 6, img: '/img/vue.png', state: "" },
+        { id: 7, img: '/img/angular.png', state: "" },
+        { id: 7, img: '/img/angular.png', state: "" },
+        { id: 8, img: '/img/nodejs.png', state: "" },
+        { id: 8, img: '/img/nodejs.png', state: "" },
+
+    ].sort(() => Math.random() - 0.5));
+
+    const [prev, setPrev] = useState(-1);
+    
+    function check(current) {
+        if(items[current].id === items[prev].id) {
+            items[current].state = "correct"
+            items[prev].state = "correct"
+            setItems([...items])
+            setPrev(-1)
+        } else {
+            items[current].state = "wrong"
+            items[prev].state = "wrong"
+            setItems([...items])
+            setTimeout(() => {
+                items[current].state = ""
+                items[prev].state = ""
+                setItems([...items])
+                setPrev(-1)
+            },1000)
+        }
+    }
+
+    function handleClick(id) {
+        if(prev === -1) {
+            items[id].state = "active"
+            setItems([...items])
+            setPrev(id)
+        } else {
+            check(id)
+        }
+    }
+
+  return (
+    <div className='container'>
+      {items.map((item, index) => {
+        return <Card key={index} item={item} id={index} handleClick={handleClick}/>
+      })}
+    </div>
+  )
+}
+
+export default Cards
